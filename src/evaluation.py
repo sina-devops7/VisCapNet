c297b94afef7231d94dd6ed3aa9343f0127f8016
Sina.Mehrabadi <sinakrh222@gmail.com>
2025-06-11 19:42:49 +0330

    Change in BLEU Score Evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,20 +1,28 @@
+# src/evaluation.py
+
+from src.caption_utils import predict_caption
 from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
+from tqdm.notebook import tqdm
 
 
 def evaluate_bleu(model, features, tokenizer, mapping, test_keys, max_length):
-    actual, predicted = list(), list()
-    for key in test_keys:
+    actual, predicted = [], []
+    smoothie = SmoothingFunction().method4
+    for key in tqdm(test_keys, desc="[INFO] Generating captions and evaluating BLEU"):
+        # Get actual captions for the image
         captions = mapping[key]
+        # Predict the caption for the image
         y_pred = predict_caption(model, features[key], tokenizer, max_length)
+        # Prepare for BLEU calculation
         actual_captions = [caption.split() for caption in captions]
         y_pred = y_pred.split()
         actual.append(actual_captions)
         predicted.append(y_pred)
-    smoothie = SmoothingFunction().method4
-    bleu_scores = {
-        "BLEU-1": corpus_bleu(actual, predicted, weights=(1.0, 0, 0, 0), smoothing_function=smoothie),
-        "BLEU-2": corpus_bleu(actual, predicted, weights=(0.5, 0.5, 0, 0), smoothing_function=smoothie),
-        "BLEU-3": corpus_bleu(actual, predicted, weights=(0.33, 0.33, 0.33, 0), smoothing_function=smoothie),
-        "BLEU-4": corpus_bleu(actual, predicted, weights=(0.25, 0.25, 0.25, 0.25), smoothing_function=smoothie),
+    # Calculate BLEU scores
+    scores = {
+        'BLEU-1': corpus_bleu(actual, predicted, weights=(1.0, 0, 0, 0), smoothing_function=smoothie),
+        'BLEU-2': corpus_bleu(actual, predicted, weights=(0.5, 0.5, 0, 0), smoothing_function=smoothie),
+        'BLEU-3': corpus_bleu(actual, predicted, weights=(0.33, 0.33, 0.33, 0), smoothing_function=smoothie),
+        'BLEU-4': corpus_bleu(actual, predicted, weights=(0.25, 0.25, 0.25, 0.25), smoothing_function=smoothie)
     }
-    return bleu_scores
+    return scores
--- src/training.py
+++ src/training.py
@@ -1,3 +1,6 @@
+from src.data_generator import data_generator
+
+
 def train_model(model, train_keys, mapping, features, tokenizer, max_length, vocab_size, batch_size, epochs, callback):
     """
     آموزش مدل روی ژنراتور دقیقا مطابق کد اصلی
